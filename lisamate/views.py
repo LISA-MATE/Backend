a7c161b2ec3e433b853976b5d4bf6083299fa378
gahyeon <1000ji1@naver.com>
2023-07-02 22:55:50 +0900

    폴더 위치 변경 및 templates 가져옴

--- lisamate/views.py
+++ lisamate/views.py
@@ -1,7 +1,7 @@
 from django.views.generic import TemplateView
 
 class MainView(TemplateView):
-    template_name = 'main.html'
+    template_name = 'mypage.html'
 
 class LoginView(TemplateView):
     template_name = 'login.html'
