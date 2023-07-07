87283192de0b503445a2f53d2fd2270bad340da2
gahyeon <1000ji1@naver.com>
2023-07-08 03:58:48 +0900

    게시물 수정 기능

--- informationPost/urls.py
+++ informationPost/urls.py
@@ -1,13 +1,15 @@
 from django.urls import path
 
-from .views import index, post_create_form_view, board_list_view, board_topic_list_view, post_detail_view
+from .views import index, post_create_form_view, board_list_view, board_topic_list_view, post_detail_view, post_update_view
 
 app_name = 'boards'
 
 urlpatterns=[
     path('', index),
+    path('new/', post_create_form_view, name='post-create'), # 게시물 작성 페이지
     path('<str:board_type>/', board_list_view, name = 'board-list'),
     path('<str:board_type>/<str:topic>/', board_topic_list_view, name='board-topic-list'),
     path('<str:board_type>/<str:topic>/<int:id>', post_detail_view, name='board-topic-detail'),
-    path('new/', post_create_form_view, name='post-create'), # 게시물 작성 페이지
+    path('<str:board_type>/<str:topic>/<int:id>/edit', post_update_view, name='post-update'),
+
 ]
--- informationPost/views.py
+++ informationPost/views.py
@@ -87,5 +87,52 @@ def post_create_form_view(request):
                 return redirect('boards:hometown-list')
     else:
         form = PostForm()
+    
+    context = {
+        'form': form,
+        'title': '글쓰기'
+    }
         
-    return render(request, 'boards/board_form.html', {'form': form})
+    return render(request, 'boards/board_form.html', context)
+
+def post_update_view(request, board_type=None, topic=None, id=None):
+    post = Post.objects.get(id=id)
+
+    if request.method == 'GET':
+        form = PostForm(instance=post)
+        context = {
+            'post': post,
+            'form': form, 
+            'title': '수정하기'
+        }
+        return render(request, 'boards/board_form.html', context)
+    else:
+        new_image = request.FILES.get('image')
+        new_file = request.FILES.get('file')
+        board_type = request.POST.get('board_type')
+        topic = request.POST.get('topic')
+        title = request.POST.get('title')
+        city = request.POST.get('city')
+        country = request.POST.get('country')
+        content = request.POST.get('content')
+
+        if new_image:
+            post.image.delete()
+            post.image = new_image
+            
+        if new_file:
+            post.file.delete()
+            post.file = new_file
+
+        post.board_type = board_type
+        post.topic = topic
+        post.title = title
+        post.city = city 
+        post.country = country
+        post.content = content
+        
+        post.save()
+
+        return redirect('boards:board-topic-detail', board_type=board_type, topic=topic, id=id)
+
+
